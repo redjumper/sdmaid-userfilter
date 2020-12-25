51e9e71a13bc8f573806b74c3f7b736cc0431222
redjumper <vmshare@gmail.com>
2020-12-25 09:54:30 +0800

    20201225

---
+++ tools/tree.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+
+echo "导出文件列表的时间可能会有点长，请耐心等待，中途不要停止脚本，导出完成会有提示"
+
+random=`uuidgen -r | cksum | cut -f 1 -d " "`
+
+if [ -f "/sbin/su" ];
+then
+	find /data/data/ -print > /storage/emulated/0/private-$random.txt
+fi
+
+if [ -d "/storage/emulated/0" ];
+then
+	find /storage/emulated/0/ \( -path /storage/emulated/0/DCIM -o -path /storage/emulated/0/Documents -o -path /storage/emulated/0/Download -o -path /storage/emulated/0/Movies -o -path /storage/emulated/0/Music -o -path /storage/emulated/0/Pictures \) -prune -o -print > /storage/emulated/0/public-$random.txt
+fi
+
+echo "导出完成"
