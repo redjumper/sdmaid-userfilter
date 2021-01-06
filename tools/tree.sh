f92b652c1f26de5bc1208a8a24152149ad00e40c
redjumper <vmshare@gmail.com>
2021-01-06 23:09:28 +0800

    /data/user/0 not used on some roms

--- tools/tree.sh
+++ tools/tree.sh
@@ -5,18 +5,34 @@ echo "导出文件列表的时间可能会有点长，请耐心等待，中途�
 start_time=`date +%s`
 random=`date +%s%N | md5sum | head -c 10`
 
+# if rooted
 if [ -f "/sbin/su" ] || [ -f "/system/bin/su" ];
 then
-	find /data/user/ -print > /storage/emulated/0/private-$random.txt
-	find /data/media/ -type d \( -name DCIM -o -name Documents -o -name Download -o -name Movies -o -name Music -o -name Pictures \) -prune -o -print  > /storage/emulated/0/public-$random.txt
-else
-    if [ -d "/storage/emulated/0" ];
-    then
-	    find /storage/emulated/0/ \( -path /storage/emulated/0/DCIM -o -path /storage/emulated/0/Documents -o -path /storage/emulated/0/Download -o -path /storage/emulated/0/Movies -o -path /storage/emulated/0/Music -o -path /storage/emulated/0/Pictures \) -prune -o -print > /storage/emulated/0/public-$random.txt
+	root=true
+	file_count=`find /data/user/0 -maxdepth 1 | wc -l`
+	if [ $file_count -lt 10 ];
+	then
+		find /data/data/ -print > /storage/emulated/0/private-$random.txt
+	else
+		find /data/user/ -print > /storage/emulated/0/private-$random.txt
+	fi
+	file_count=`find /data/media/0 -maxdepth 1 | wc -l`
+	if [ $file_count -lt 10 ];
+	then
+		find /storage/emulated/0/ \( -path /storage/emulated/0/DCIM -o -path /storage/emulated/0/Documents -o -path /storage/emulated/0/Download -o -path /storage/emulated/0/Movies -o -path /storage/emulated/0/Music -o -path /storage/emulated/0/Pictures \) -prune -o -print > /storage/emulated/0/public-$random.txt
 		sed -i 's/^\/storage\/emulated/\/data\/media/' public-$random.txt
-    fi
+	else
+		find /data/media/ -type d \( -name DCIM -o -name Documents -o -name Download -o -name Movies -o -name Music -o -name Pictures \) -prune -o -print  > /storage/emulated/0/public-$random.txt
+	fi
 fi
 
+# if not rooted
+if [ "$root" = false ] && [ -d "/storage/emulated/0" ];
+then
+	find /storage/emulated/0/ \( -path /storage/emulated/0/DCIM -o -path /storage/emulated/0/Documents -o -path /storage/emulated/0/Download -o -path /storage/emulated/0/Movies -o -path /storage/emulated/0/Music -o -path /storage/emulated/0/Pictures \) -prune -o -print > /storage/emulated/0/public-$random.txt
+	sed -i 's/^\/storage\/emulated/\/data\/media/' public-$random.txt
+fi
+	
 end_time=`date +%s`
 execute_time=`expr $end_time - $start_time`
 
